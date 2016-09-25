c3944c155ea26af152ef58b766c673352c705540
antoine drabble <antoine drabble>
2016-09-25 17:29:06 +0200

    Fix node.js app

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
-
-
+/**
+  * Project : TWEB labo 1
+  * Author : Antoine Drabble & Guillaume Serneels
+  * Date : 25.09.2016
+  * Description : 
+  *
+*/
 var express = require('express');
 var app = express();
 
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'))
 app.set('port', (process.env.PORT || 5000));
 
-app.engine('html', require('ejs').renderFile);
-
-app.set('view engine', 'ejs');
-
-app.get('/', function (req, res)
-{
-    res.render('index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(app.get('port'), function() {
